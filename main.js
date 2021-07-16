696211c2977ede8f7609728116839fa244c8639c
PrakeekCodez <83758451+PrakeekCodez@users.noreply.github.com>
2021-07-16 18:16:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,44 +4,42 @@ difference = 0;
 rightWristX = 0;
 leftWristX = 0;
 
-  function setup() {
+function setup(){
   video = createCapture(VIDEO);
-  video.size(550, 500);
+  video.size(550,500);
 
-  canvas = createCanvas(550, 550);
-  canvas.position(560,150);
-
+  canvas = createCanvas(550,550);
+  canvas.postion(560,150);
   poseNet = ml5.poseNet(video, modelLoaded);
   poseNet.on('pose', gotPoses);
 }
 
-function modelLoaded() {
-  console.log('PoseNet Is Initialized!');
+
+function modelLoaded(){
+  console.log('PoseNet Is Inititialized!');
 }
 
 
-function gotPoses(results)
-{
-  if(results.length > 0)
-  {
-    console.log(results);
-    noseX = results[0].pose.nose.x;
-    noseY = results[0].pose.nose.y;
-    console.log("noseX = " + noseX +" noseY = " + noseY);
+function draw(){
+  background("#969A97");
 
-    leftWristX = results[0].pose.leftWrist.x;
-    rightWristX = results[0].pose.rightWrist.x;
-    difference = floor(leftWristX - rightWristX);
-
-    console.log("leftWristX  = " + leftWristX  + " rightWristX = "+ rightWristX + " difference = " + difference);
-  }
-}
-
-function draw() {
-background('#969A97');
-
-  document.getElementById("square_side").innerHTML = "Width And Height of a Square will be = " + difference +"px";
+  document.getElementById("square_side").innerHTML = "Width And Hieght Of A Square Will Be = " + difference +"px"
   fill('#F90093');
   stroke('#F90093');
   square(noseX, noseY, difference);
+}
+
+function gotPoses (results)
+{
+ if(results.length > 0)
+ {
+  console.log(results);
+  noseX = results[0].pose.nose.x;
+  noseY = results[0].pose.nose.y;
+  console.log("noseX = " + noseY +" noseY = " + noseY);
+  leftWristX = results[0].pose.leftWristX.x;
+  rightWristX = results[0].pose.rightWristX.x; 
+  difference = floor(leftWristX - rightWristX);
+  console.log("leftWristX = " + leftWristX +" rightWristX = " + rightWristX + " difference = " + difference);
+ }
 }
